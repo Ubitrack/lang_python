e622132ff679c55ace0a7dfa07b8c43b3a9bd828
Ulrich Eck <ulrich.eck@tum.de>
2019-01-29 18:58:02 +0100

    commented out imports in conanfile

--- conanfile.py
+++ conanfile.py
@@ -36,10 +36,10 @@ class UbitrackCoreConan(ConanFile):
         self.options['Boost'].without_python = True
         self.options['python_dev_config'].python = self.options.python
 
-    def imports(self):
-        self.copy(pattern="*.dll", dst="bin", src="bin") # From bin to bin
-        self.copy(pattern="*.dylib*", dst="lib", src="lib") 
-        self.copy(pattern="*.so*", dst="lib", src="lib") 
+    # def imports(self):
+    #     self.copy(pattern="*.dll", dst="bin", src="bin") # From bin to bin
+    #     self.copy(pattern="*.dylib*", dst="lib", src="lib") 
+    #     self.copy(pattern="*.so*", dst="lib", src="lib") 
        
     def build(self):
         cmake = CMake(self)
